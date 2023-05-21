342a24f8616c4040df5e39156068f9f2e078aa0f
Reinis Taukulis <reinis.taukulis@gmail.com>
2023-05-21 16:40:02 +0300

    Fix build.zig

--- build.zig
+++ build.zig
@@ -3,7 +3,7 @@ const std = @import("std");
 const FileSource = std.build.FileSource;
 
 pub fn build(b: *std.Build) void {
-    b.addModule("handmade_gl", .{
+    _ = b.addModule("handmade_gl", .{
         .source_file = FileSource.relative("src/gl.zig"),
     });
 }
